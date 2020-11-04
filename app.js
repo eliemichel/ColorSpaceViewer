19603235720b0d6746b7c8f84a8bddda9ce1b826
Elie Michel <elie.michel@exppad.com>
2020-11-04 10:17:54 +0100

    add cube viewer

--- app.js
+++ app.js
@@ -11,27 +11,27 @@ animate();
 
 function getImageData( image ) {
 
-    var canvas = document.createElement( 'canvas' );
-    canvas.width = image.width;
-    canvas.height = image.height;
+	var canvas = document.createElement( 'canvas' );
+	canvas.width = image.width;
+	canvas.height = image.height;
 
-    var context = canvas.getContext( '2d' );
-    context.drawImage( image, 0, 0 );
+	var context = canvas.getContext( '2d' );
+	context.drawImage( image, 0, 0 );
 
-    return context.getImageData( 0, 0, image.width, image.height );
+	return context.getImageData( 0, 0, image.width, image.height );
 
 }
 
 function getPixel( imagedata, x, y ) {
 
-    var position = ( x + imagedata.width * y ) * 4
-    var data = imagedata.data;
-    return {
-    	r: data[ position ],
-    	g: data[ position + 1 ],
-    	b: data[ position + 2 ],
-    	a: data[ position + 3 ]
-    };
+	var position = ( x + imagedata.width * y ) * 4
+	var data = imagedata.data;
+	return {
+		r: data[ position ],
+		g: data[ position + 1 ],
+		b: data[ position + 2 ],
+		a: data[ position + 3 ]
+	};
 }
 
 function initPointCloud(image) {
@@ -80,6 +80,81 @@ function initPointCloud(image) {
 	return geometry;
 }
 
+function initPointCloudLut(cube) {
+	var rawLines = cube.split("\n");
+	var lines = [];
+	var l;
+	for (var i = 0 ; i < rawLines.length ; ++i) {
+		l = rawLines[i];
+		if (l.trim().startsWith("#") || l.trim() == "") continue;
+		lines.push(l);
+	}
+	var c = 0;
+	var tokens;
+	var size = undefined;
+	for (; size === undefined && c < lines.length ; ++c) {
+		if (lines[c].startsWith("LUT_3D_SIZE")) {
+			tokens = lines[c].trim().split(" ");
+			size = parseInt(tokens[tokens.length - 1]);
+		}
+	}
+
+	if (size === undefined) {
+		console.logWarning("Could not find LUT_3D_SIZE");
+		return;
+	}
+
+	console.log("size = " + size);
+	var particleCount = 6 * size * size - 12 * size - 8;
+
+	var geometry = new THREE.BufferGeometry();
+
+	var positions = [];
+	var colors = [];
+
+	var color = new THREE.Color();
+
+	var x, y, z;
+	var r, g, b;
+	for (var k = 0; k < size; ++k ) {
+		for (var j = 0; j < size; ++j ) {
+			for (var i = 0; i < size; ++i ) {
+				var maxi = Math.max(Math.max(i, j), k);
+				var mini = Math.min(Math.min(i, j), k);
+				if (maxi == size - 1 || mini == 0) {
+					/*
+					x = (i / (size - 1) - 0.5) * 256;
+					y = (j / (size - 1) - 0.5) * 256;
+					z = (k / (size - 1) - 0.5) * 256;
+					tokens = lines[c].split(" ");
+					r = parseFloat(tokens[0]);
+					g = parseFloat(tokens[1]);
+					b = parseFloat(tokens[2]);
+					*/
+					r = i / (size - 1);
+					g = j / (size - 1);
+					b = k / (size - 1);
+					tokens = lines[c].split(" ");
+					z = (parseFloat(tokens[0]) - 0.5) * 256;
+					y = (parseFloat(tokens[1]) - 0.5) * 256;
+					x = (parseFloat(tokens[2]) - 0.5) * 256;
+					
+					positions.push(x, y, z);
+					colors.push(r, g, b);
+				}
+				++c;
+			}
+		}
+	}
+
+	geometry.setAttribute( 'position', new THREE.Float32BufferAttribute( positions, 3 ) );
+	geometry.setAttribute( 'color', new THREE.Float32BufferAttribute( colors, 3 ) );
+
+	geometry.computeBoundingSphere();
+
+	return geometry;
+}
+
 function rebuildScene(texture) {
 	{
 		if (pointsObject !== undefined) {
@@ -103,6 +178,24 @@ function rebuildScene(texture) {
 		planeObject.scale.x *= 0.5;
 		planeObject.scale.y *= 0.5;
 		scene2.add( planeObject );
+	}
+}
+
+function rebuildSceneLut(cube) {
+	{
+		if (pointsObject !== undefined) {
+			scene.remove(pointsObject);
+		}
+		var geometry = initPointCloudLut(cube);
+		var material = new THREE.PointsMaterial( { size: guiData.pointSize, vertexColors: true } );
+		pointsObject = new THREE.Points( geometry, material );
+		scene.add( pointsObject );
+	}
+
+	{
+		if (planeObject !== undefined) {
+			scene2.remove(planeObject);
+		}
 	}
 }
 
@@ -234,19 +327,31 @@ function init() {
 }
 
 Handlers.prototype.userFileChanged = function(userFile) {
-	var reader = new FileReader();
+	var filename = userFile.files[0];
 
-    reader.onload = function(e) {
-    	var img = document.createElement('img');
-    	img.onload = function(e) {
-		    var texture = new THREE.Texture( this );
-		    texture.needsUpdate = true;
-		    rebuildScene(texture);
-		};
-    	img.src = e.target.result;
-    }
+	if (filename.name.toLowerCase().endsWith(".cube")) {
+		var reader = new FileReader();
 
-    reader.readAsDataURL(userFile.files[0]);
+		reader.onload = function(e) {
+			rebuildSceneLut(e.target.result);
+		}
+
+		reader.readAsText(filename);
+	} else {
+		var reader = new FileReader();
+
+		reader.onload = function(e) {
+			var img = document.createElement('img');
+			img.onload = function(e) {
+				var texture = new THREE.Texture( this );
+				texture.needsUpdate = true;
+				rebuildScene(texture);
+			};
+			img.src = e.target.result;
+		}
+
+		reader.readAsDataURL(filename);
+	}
 }
 
 function onWindowResize() {
